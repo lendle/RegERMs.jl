4a0665c31c445da671cefb8173dca1123b2d173e
Christoph Sawade <christoph@sawade.me>
2014-07-25 17:12:14 +0200

    [WIP] Multinomial logistic regression
    This is an example to clarify the structure of RegERMs (e.g., losses).
    
    * Implement multinomial logistic regression
    - version of loss and model functions expect (class-1) parameter vectors (see, e.g., [1])
    - add tests (WIP)
    
    * Structural changes:
    - introduce abstract multinomial loss
    - add regression_function in addition to model: model takes care of the
      type of the model (ordinal, binomial, multinomial) and provides
    appropriate gradients and values, and predictions, whereas
    regression_function allows the distinction between, e.g., kernelized and
    linear models.
    - renaming w to theta
    
    * Disabled Mercer map for focus
    
    [1]
    http://en.wikipedia.org/wiki/Multinomial_logistic_regression#As_a_set_of_independent_binary_regressions

--- src/RegERMs.jl
+++ src/RegERMs.jl
@@ -4,38 +4,50 @@ using StatsBase
 using Optim
 
 import StatsBase: predict
-import Optim: optimize 
+import Optim: optimize
 
 ## Exports
 
-export 
+export
     # loss
-    Loss,           # abstract type for all kinds of loss
-    LogisticLoss,   # Type to represent logistic loss
-    SquaredLoss,    # Type to represent squared loss
-    HingeLoss,      # Type to represent hinge loss
+    Loss,                       # abstract type for all kinds of loss
+    LogisticLoss,               # Type to represent logistic loss
+    SquaredLoss,                # Type to represent squared loss
+    HingeLoss,                  # Type to represent hinge loss
+    MultinomialLogisticLoss,    # Type to represent multinomial logistic loss (soft max)
 
-    value,              # evaluate a single value
-    values,             # evaluate multiple values
-    deriv,              # evaluate a single derivative value
-    derivs,             # evaluate multiple derivative values
-    value_and_deriv,    # jointly evaluate function value and derivative
-    tloss,              # the total loss 
+    value,                      # evaluate a single value
+    values,                     # evaluate multiple values
+    deriv,                      # evaluate a single derivative value
+    derivs,                     # evaluate multiple derivative values
+    value_and_deriv,            # jointly evaluate function value and derivative
+    tloss,                      # the total loss
 
     # regularizer
-    Regularizer,    # abstract type for all kinds of regularizers
-    L2reg,          # Type to represent L2 regularizer
+    Regularizer,                # abstract type for all kinds of regularizers
+    L2reg,                      # Type to represent L2 regularizer
 
     # model
-    Model, 
-    PrimalModel, 
-    DualModel, 
+    Model,
+    RegressionModel,
+    OrdinalModel,
+    BinomialModel,
+    MultinomialModel,
     predict,
+    values,
+    gradient,
+
+    # regression function
+    RegressionFunction,
+    LinearRegressionFunction,
+    value,
+    grad,
 
     # models
     SVM,
     RidgeReg,
-    LogReg,
+    BinomialLogReg,
+    MultinomialLogReg,
 
     # solvers
     LBFGSSolver,
@@ -43,7 +55,8 @@ export
 
     # optim
     RegERM, 
-    RegressionSolver, 
+    RegressionSolver,
+    objective,
     optimize
 
 ## Source files
@@ -52,6 +65,7 @@ export
 include("loss.jl")
 include("regularizer.jl")
 include("mercer_map.jl")   ## TODO: need some discussion about the API for this
+include("regression_function.jl")
 include("model.jl")
 include("optim.jl")
 
